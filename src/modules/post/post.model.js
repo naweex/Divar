c36f66468ef9adb8e4c49f6808c9ab9a694e1b5e
navid bahrami <navidbahrami1996@gmail.com>
2024-09-25 20:27:24 +0330

    post files

--- src/modules/post/post.model.js
+++ src/modules/post/post.model.js
@@ -1,12 +1,20 @@
-const { Schema, Types } = require("mongoose");
+const {Schema, Types, model} = require("mongoose");
 
 const PostSchema = new Schema({
-    title : {type : String , required : true} ,
-    content : {type : String , required : true} ,
-    category : {type : Types.ObjectId , ref : 'category' , required : true} ,
-    province : {type : String , required : true} ,
-    city :  {type : String , required : true} ,
-    district :  {type : String , required : true} ,
-    coordinate :  {type : [Number] , required : true} ,
-    images : {type : [Number] , required : false , default : []} ,
-})
+    title: {type: String, required: true},
+    userId: {type: Types.ObjectId, required: true},
+    amount: {type: Number, required: true, default: 0},
+    content: {type: String, required: true},
+    category: {type: Types.ObjectId, ref: "Category", required: true},
+    province: {type: String, required: false},
+    city: {type: String, required: false},
+    district: {type: String, required: false},
+    address: {type: String, required: false},
+    coordinate: {type: [Number], required: true}, //51.215485487, 52.687524154
+    images: {type: [String], required: false, default: []},
+    options: {type: Object, default: {}}
+}, {
+    timestamps: true
+});
+const PostModel = model("post", PostSchema);
+module.exports = PostModel;
