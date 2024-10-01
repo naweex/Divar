a6781d8816c53fcdc229a4d244abcf8dec238379
navid bahrami <navidbahrami1996@gmail.com>
2024-10-01 12:03:02 +0330

    commit

--- src/config/mongoose.config.js
+++ src/config/mongoose.config.js
@@ -1,11 +1,9 @@
-const {default : mongoose} = require('mongoose')
-const dotenv = require('dotenv')
+const { default: mongoose } = require("mongoose");
+const dotenv = require("dotenv");
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
-    console.log('connect to DB');
-    
+    console.log("connected to DB.");
 }).catch(err => {
-    console.log(err?.message ?? 'failed to connect mongodb');
-    
+    console.log(err?.message ?? "Failed DB connection");
 })
