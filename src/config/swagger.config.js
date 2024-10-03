87dc92845fc7e0aa791c90aedda0636c04ed2d2d
navid bahrami <navidbahrami1996@gmail.com>
2024-10-03 13:30:07 +0330

    swagger

--- src/config/swagger.config.js
+++ src/config/swagger.config.js
@@ -1,25 +1,19 @@
-const swaggerJsDoc = require('swagger-jsdoc')
-const swaggerUi = require('swagger-ui-express')
-
-function swaggerConfig(app) {
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+function SwaggerConfig (app) {
     const swaggerDocument = swaggerJsDoc({
-    swaggerDefinition : {
-        openapi : '3.0.1' ,
-        info : {
-            title : 'Divar' ,
-            description : 'Divar project' ,
-            version : '1.0.0' ,
-            
-        }
-    },
-    apis : [process.cwd() + '/src/modules/**/*.swagger.js']
-})
-const swagger = swaggerUi.setup(swaggerDocument , {})
-app.use('/swagger' , swaggerUi.serve ,swagger)
+        swaggerDefinition: {
+            openapi: "3.0.1",
+            info: {
+                title: "divar-backend",
+                description: "botostart nodejs course",
+                version: "1.0.0",
+            },
+
+        },
+        apis: [process.cwd() + "/src/modules/**/*.swagger.js"]
+    });
+    const swagger = swaggerUi.setup(swaggerDocument, {});
+    app.use("/swagger", swaggerUi.serve, swagger);
 }
-
-
-
-
-
-module.exports = swaggerConfig
+module.exports = SwaggerConfig;
